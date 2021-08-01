3fca706ff22d02563fe01305b18e926b34992501
Nikita Zagorsky <toltenos@gmail.com>
2021-08-02 02:44:23 +0300

    guide update

--- remote-setup.sh
+++ remote-setup.sh
@@ -1,5 +1,7 @@
 # Usage: 
-# curl -sfL git.io/JB9ki | sh -
+# curl -sfL git.io/JB9ki > run.sh
+# bash run.sh
+
 curl --silent https://raw.githubusercontent.com/nzagorsky/arch-init/master/bootstrap.sh --output /root/bootstrap.sh
 curl --silent https://raw.githubusercontent.com/nzagorsky/arch-init/master/arch-post.sh --output /root/arch-post.sh
 
