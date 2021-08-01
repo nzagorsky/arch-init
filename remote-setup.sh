5a7bc9c732a09ed8b3c21ae2e6db3c282c8e8b7f
Nikita Zagorsky <toltenos@gmail.com>
2021-08-02 02:21:11 +0300

    Add remote setup script

---
+++ remote-setup.sh
@@ -0,0 +1,4 @@
+curl --silent https://raw.githubusercontent.com/nzagorsky/arch-init/master/bootstrap.sh /root/bootstrap.sh
+curl --silent https://raw.githubusercontent.com/nzagorsky/arch-init/master/arch-post.sh /root/arch-post.sh
+
+bash bootstrap.sh
